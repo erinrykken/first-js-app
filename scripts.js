1e70c278918735085a867d057895fa2ba14763d0
erinrykken <139806358+erinrykken@users.noreply.github.com>
2023-10-02 13:40:44 -0600

    Added files

---
+++ scripts.js
@@ -0,0 +1,13 @@
+//practice math
+
+/*let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize);*/
+
+let size = 200;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
