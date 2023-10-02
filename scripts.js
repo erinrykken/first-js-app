c7d376bb17bd318e49636e91c4d0525c67bc7115
erinrykken <139806358+erinrykken@users.noreply.github.com>
2023-10-02 17:34:25 -0600

    Update scripts.js with favoriteFood variable

--- scripts.js
+++ scripts.js
@@ -5,9 +5,12 @@ document.write(simpleAddition);
 
 let size = 100;
 let doubleSize = size * 2;
-document.write(doubleSize);*/
+document.write(doubleSize);
 
 let size = 200;
 let doubleSize = size * 2;
 let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
+document.write(minSize);*/
+
+let favoriteFood = 'potatoes';
+document.write(favoriteFood);
