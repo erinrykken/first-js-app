7e7aac80eba0c532553888329feb4f839ef29c96
erinrykken <139806358+erinrykken@users.noreply.github.com>
2023-10-02 17:39:35 -0600

    Deleted practice math

--- scripts.js
+++ scripts.js
@@ -1,16 +1,2 @@
-//practice math
-
-/*let simpleAddition = 2 + 2;
-document.write(simpleAddition);
-
-let size = 100;
-let doubleSize = size * 2;
-document.write(doubleSize);
-
-let size = 200;
-let doubleSize = size * 2;
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);*/
-
 let favoriteFood = 'potatoes';
 document.write(favoriteFood);
