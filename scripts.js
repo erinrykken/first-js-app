d2714e59a8e6bc8e6f9e565b921531eef0dfa1c6
erinrykken <139806358+erinrykken@users.noreply.github.com>
2023-10-04 18:01:11 -0600

    Wrote first 3 lines of pokemonList

--- scripts.js
+++ scripts.js
@@ -1,2 +1,8 @@
-let favoriteFood = 'potatoes';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Butterfree', height: 1.1, type: ['Bug', 'Rock', 'Grass']}
+    {name: 'Caterpie', height: 0.3, type: ['Flying', 'Rock', 'Fire']}
+    {name: 'Charmander', height: 0.6, type: ['Fairy', 'Fire', 'Ice']}
+];
+
+
+/*name, height, type*/
