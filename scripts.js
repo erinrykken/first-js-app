122e8213f238662637bdf6d0dc365dfba4b9c1ae
erinrykken <139806358+erinrykken@users.noreply.github.com>
2023-10-21 14:01:06 -0600

    Added commas to create array of objects

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 /*name, height, type*/
 
 let pokemonList = [
-    {name: 'Butterfree', height: 1.1, type: ['Bug', 'Rock', 'Grass']}
-    {name: 'Caterpie', height: 0.3, type: ['Flying', 'Rock', 'Fire']}
+    {name: 'Butterfree', height: 1.1, type: ['Bug', 'Rock', 'Grass']},
+    {name: 'Caterpie', height: 0.3, type: ['Flying', 'Rock', 'Fire']},
     {name: 'Charmander', height: 0.6, type: ['Fairy', 'Fire', 'Ice']}
 ];
 
