c3cf9b3befe76ae5c9e5a1db1666bbe35c7bbb9b
erinrykken <139806358+erinrykken@users.noreply.github.com>
2023-10-29 17:05:42 -0600

    Made updates for assignment

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+//returns "[pokemone name]: [number] units tall" 
+
+    let pokemonList = [
+        {name: 'Butterfree', height: 1.1, type: ['Bug', 'Rock', 'Grass']},
+        {name: 'Caterpie', height: 0.3, type: ['Flying', 'Rock', 'Fire']},
+        {name: 'Charmander', height: 0.6, type: ['Fairy', 'Fire', 'Ice']},
+    ] 
+    for (let i = 0; i <= 2; i++) {
+        document.write((pokemonList[i].name + ': ') + (pokemonList[i].height) + ' units tall');
+    if (pokemonList[i].height > 1.0){
+            document.write('I am a big feller!');
+         }      
+    }
+    
+
+
+//practice for part 3 of exercise
+
+//if the pokemon height is above 1.0 then write "Big Feller!" after the height
