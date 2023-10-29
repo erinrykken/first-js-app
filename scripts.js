0d56a78b833d9b4701ca919b3a1addd7ccb66a32
erinrykken <139806358+erinrykken@users.noreply.github.com>
2023-10-29 15:25:49 -0600

    updates to for code

--- scripts.js
+++ scripts.js
@@ -50,12 +50,12 @@ for (let i = 0; i <= pokemonList.length; i++) {
     let pokemonList = [
         {name: 'Butterfree', height: 1.1, type: ['Bug', 'Rock', 'Grass']},
         {name: 'Caterpie', height: 0.3, type: ['Flying', 'Rock', 'Fire']},
-        {name: 'Charmander', height: 0.6, type: ['Fairy', 'Fire', 'Ice']}
+        {name: 'Charmander', height: 0.6, type: ['Fairy', 'Fire', 'Ice']},
     ] 
     for (let i = 0; i <= 2; i++) {  
         console.log('My name is ' + pokemonList[i].name);
         console.log('I am ' + pokemonList[i].height);
-        console.log('My types are' + pokemonList[i].type[0][1][2])
+        console.log('My types are ' + pokemonList[i].type[0], pokemonList[i].type[1], pokemonList[i].type[2]);
     }
 
     
