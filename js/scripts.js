dc3322128abad3015f63c28ad1c96b23d60b8389
erinrykken <139806358+erinrykken@users.noreply.github.com>
2023-10-30 18:09:49 -0600

    Added if else statements

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,20 @@
         {name: 'Charmander', height: 0.6, type: ['Fairy', 'Fire', 'Ice']},
     ] 
     for (let i = 0; i <= 2; i++) {
-        document.write((pokemonList[i].name + ': ') + (pokemonList[i].height) + ' units tall');
-    if (pokemonList[i].height > 1.0){
-            document.write('I am a big feller!');
-         }      
+        document.write((pokemonList[i].name + ': ') + (pokemonList[i].height) + ' units tall    ');
+    if (pokemonList[i].height >= 1.0){
+            document.write("  --  I'm a big fella!  ");
     }
+    else if (pokemonList[i].height >= 0.5 && pokemonList[i].height < 1.0){
+        document.write("  --  I'm a medium fella!  ");
+    } 
+    else {
+        document.write("  --  I'm a little fella!  ");
+    }
+}
+
+
+
     
 
 
