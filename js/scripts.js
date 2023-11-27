c8ad7d56b2e5210e6128266e1113fa36b3f2ca58
erinrykken <139806358+erinrykken@users.noreply.github.com>
2023-11-27 09:47:51 -0700

    formatted as <ul> and <li>

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,12 @@
         {name: 'Caterpie', height: 0.3, type: ['Flying', 'Rock', 'Fire']},
         {name: 'Charmander', height: 0.6, type: ['Fairy', 'Fire', 'Ice']},
     ] 
+    //start
+    document.write('<ul>')
     for (let i = 0; i <= 2; i++) {
-        document.write((pokemonList[i].name + ': ') + (pokemonList[i].height) + ' units tall    ');
+        document.write('<li>' + (pokemonList[i].name + ': ') + (pokemonList[i].height) + ' units tall    ');
     if (pokemonList[i].height >= 1.0){
-            document.write("  --  I'm a big fella!  ");
+            document.write("  --  I'm a big fella!  ") ;
     }
     else if (pokemonList[i].height >= 0.5 && pokemonList[i].height < 1.0){
         document.write("  --  I'm a medium fella!  ");
@@ -16,8 +18,9 @@
     else {
         document.write("  --  I'm a little fella!  ");
     }
+    document.write('</li>')
 }
-
+document.write('</ul>')
 
 
     
