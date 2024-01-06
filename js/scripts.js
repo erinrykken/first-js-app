1a04e811ebdb489c79531c4c3c7ffbe7b8309ec0
erinrykken <139806358+erinrykken@users.noreply.github.com>
2024-01-06 09:36:42 -0700

    forEach updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
-//forEach loop, returns Name is X units tall. They are a XYZ.
+//forEach loop, returns Name is X units tall. They are a X, a Y, and a Z.
 
-    let pokemonList = [
+let pokemonList = [
         {name: 'Butterfree', height: 1.1, type: ['Bug', 'Rock', 'Grass']},
         {name: 'Caterpie', height: 0.3, type: ['Flying', 'Rock', 'Fire']},
         {name: 'Charmander', height: 0.6, type: ['Fairy', 'Fire', 'Ice']},
     ]
-    function myLoopFunction(pokemon) {
-        console.log(pokemon.name + ' is ' + pokemon.height + ' units tall. ' + 'They are a ' + pokemon.type + '.');
-      }
-      pokemonList.forEach(myLoopFunction);
+function myLoopFunction(pokemon) {
+  console.log(pokemon.name + ' is ' + pokemon.height + ' units tall. ' + 'They are a ' + pokemon.type[0] + ', a ' + pokemon.type[1] + ', and a ' + pokemon.type[2] + '.');
+}
+pokemonList.forEach(myLoopFunction);
 
 //for loop - returns "[pokemone name]: [number] units tall" 
 /*document.write('<ul>')
