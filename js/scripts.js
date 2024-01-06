72908b252edd785f0c59afef692d5f1ac1ed569a
erinrykken <139806358+erinrykken@users.noreply.github.com>
2024-01-06 09:13:02 -0700

    Update for loop to forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,24 @@
         {name: 'Butterfree', height: 1.1, type: ['Bug', 'Rock', 'Grass']},
         {name: 'Caterpie', height: 0.3, type: ['Flying', 'Rock', 'Fire']},
         {name: 'Charmander', height: 0.6, type: ['Fairy', 'Fire', 'Ice']},
-    ] 
-    //start
-    document.write('<ul>')
+    ]
+    function myLoopFunction(pokemon) {
+        console.log(pokemon.name + ' is ' + pokemon.height + ' units tall. ' + 'They are a ' + pokemon.type.);
+      }
+      pokemonList.forEach(myLoopFunction);
+
+    //for loop 
+    /*document.write('<ul>')
     for (let i = 0; i <= 2; i++) {
         document.write('<li>' + (pokemonList[i].name + ': ') + (pokemonList[i].height) + ' units tall    ');
-    if (pokemonList[i].height >= 1.0){
+   
+     //forEach loop   
+     /*pokemonList.forEach(function(name) {
+            console.log(name);
+          });    
+   
+   
+          if (pokemonList[i].height >= 1.0){
             document.write("  --  I'm a big fella!  ");
     }
     else if (pokemonList[i].height >= 0.5 && pokemonList[i].height < 1.0){
@@ -19,6 +31,6 @@
         document.write("  --  I'm a little fella!  ");
     }
     document.write('</li>')
-}
-document.write('</ul>')
+    */
+
 
