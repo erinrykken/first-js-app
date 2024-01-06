16325487b5bd79c2b28aedcfa9a03a493017279a
erinrykken <139806358+erinrykken@users.noreply.github.com>
2024-01-06 11:36:44 -0700

    Note formatting update

--- js/scripts.js
+++ js/scripts.js
@@ -10,17 +10,14 @@ function myLoopFunction(pokemon) {
 }
 pokemonList.forEach(myLoopFunction);
 
-//for loop - returns "[pokemone name]: [number] units tall" 
-/*document.write('<ul>')
+/*
+for loop - returns "[pokemone name]: [number] units tall" 
+document.write('<ul>')
     
     for (let i = 0; i <= 2; i++) {
         document.write('<li>' + (pokemonList[i].name + ': ') + (pokemonList[i].height) + ' units tall    ');
-   
-//forEach loop   
-     /*pokemonList.forEach(function(name) {
-            console.log(name);
-          });    
-   
+
+previous for loop if else statement for age
           if (pokemonList[i].height >= 1.0){
             document.write("  --  I'm a big fella!  ");
     }
@@ -31,6 +28,11 @@ pokemonList.forEach(myLoopFunction);
         document.write("  --  I'm a little fella!  ");
     }
     document.write('</li>')
+   
+forEach loop   
+     /*pokemonList.forEach(function(name) {
+            console.log(name);
+          });    
     */
 
 
