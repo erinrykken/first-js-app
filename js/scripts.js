efe65977963bf6e593b92bf2bd0c27b08bb6ed04
erinrykken <139806358+erinrykken@users.noreply.github.com>
2024-01-06 09:18:15 -0700

    More forEach changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-//returns "[pokemone name]: [number] units tall" 
+//forEach loop, returns Name is X units tall. They are a XYZ.
 
     let pokemonList = [
         {name: 'Butterfree', height: 1.1, type: ['Bug', 'Rock', 'Grass']},
@@ -6,20 +6,20 @@
         {name: 'Charmander', height: 0.6, type: ['Fairy', 'Fire', 'Ice']},
     ]
     function myLoopFunction(pokemon) {
-        console.log(pokemon.name + ' is ' + pokemon.height + ' units tall. ' + 'They are a ' + pokemon.type.);
+        console.log(pokemon.name + ' is ' + pokemon.height + ' units tall. ' + 'They are a ' + pokemon.type + '.');
       }
       pokemonList.forEach(myLoopFunction);
 
-    //for loop 
-    /*document.write('<ul>')
+//for loop - returns "[pokemone name]: [number] units tall" 
+/*document.write('<ul>')
+    
     for (let i = 0; i <= 2; i++) {
         document.write('<li>' + (pokemonList[i].name + ': ') + (pokemonList[i].height) + ' units tall    ');
    
-     //forEach loop   
+//forEach loop   
      /*pokemonList.forEach(function(name) {
             console.log(name);
           });    
-   
    
           if (pokemonList[i].height >= 1.0){
             document.write("  --  I'm a big fella!  ");
